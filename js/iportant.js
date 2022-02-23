9366500cd602d83863881b7a8dabd5bb6ecb2781
Fauli1221 <43648117+Fauli1221@users.noreply.github.com>
2022-02-23 21:48:38 +0100

    Update iportant.js

--- js/iportant.js
+++ js/iportant.js
@@ -1,3 +1,3 @@
 window.addEventListener('load', function () {
-    window.location.assign("https://www.therickroll.com");
+    window.location.assign("https://www.youtube.com/watch?v=hDJcMk4e0rc");
 })
