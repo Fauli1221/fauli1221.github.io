e4c1a59cb8a87b7f5546f51722282a4408d0256e
Fauli1221 <43648117+Fauli1221@users.noreply.github.com>
2022-02-23 22:17:26 +0100

    Update iportant.js

--- js/iportant.js
+++ js/iportant.js
@@ -1,4 +1,4 @@
-window.addEventListener('load', function () {
+window.addEventListener('load', function async () {
     setTimeout(function () {
         window.open('https://www.youtube.com/watch?v=hDJcMk4e0rc', '_blank');
     }, 10000);
