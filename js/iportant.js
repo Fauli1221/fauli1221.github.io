0a77657176ca0998b221ca5abe693f4b66e10d5e
Fauli1221 <43648117+Fauli1221@users.noreply.github.com>
2022-02-23 22:18:30 +0100

    Update iportant.js

--- js/iportant.js
+++ js/iportant.js
@@ -1,5 +1,7 @@
+const delay = ms => new Promise(res => setTimeout(res, ms));
+
 window.addEventListener('load', function async () {
-    setTimeout(function () {
-        window.open('https://www.youtube.com/watch?v=hDJcMk4e0rc', '_blank');
-    }, 10000);
+    await delay(5000);
+    window.open('https://www.youtube.com/watch?v=hDJcMk4e0rc', '_blank');
+    
 })
